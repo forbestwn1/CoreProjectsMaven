4eaff12cd2c1efa241c41eb1427c629374a6fe0f
forbestwn1 <forbestwn@gmail.com>
2026-07-05 03:50:10 -0400

    1111111

--- appweb/StoryDesignWizzard/src/QuestionairGroupResponseData.jsx
+++ appweb/StoryDesignWizzard/src/QuestionairGroupResponseData.jsx
@@ -11,6 +11,8 @@ export default function QuestionairGroupResponseData({ questionair, onChange }) 
   var isShowQ = questionairUtility.getChildQuestionairByTag(questionair, node_COMMONCONSTANT.STORYDESIGN_QUESTION_TAG_DATASOURCERESPONSEDATAISSHOW);
   var dataInfoQ = questionairUtility.getChildQuestionairByTag(questionair, node_COMMONCONSTANT.STORYDESIGN_QUESTION_TAG_ENTITYINFO);
   const dataInfo = questionairUtility.getValueFromQuestionairItem(dataInfoQ)[node_COMMONATRIBUTECONSTANT.STORYWIZZARDQUESTIONVALUEDATASOURCEENTITYINFOSTATIC_ENTITYINFO];
+  var childrenGroupQ = questionairUtility.getChildQuestionairByTag(questionair, node_COMMONCONSTANT.STORYDESIGN_QUESTION_TAG_DATASOURCERESPONSEDATACHILDREN);
+  var childItemsQ = childrenGroupQ==undefined? [] : childrenGroupQ[node_COMMONATRIBUTECONSTANT.STORYWIZZARDQUESTIONAIR_ITEM];
 
   const selected = questionairUtility.getValueFromQuestionairItem(isShowQ)[node_COMMONATRIBUTECONSTANT.STORYWIZZARDQUESTIONVALUEDATASOURCERESPONSEPARMCHOOSEISSHOWDYNAMIC_ISSHOW];
 
@@ -29,6 +31,11 @@ export default function QuestionairGroupResponseData({ questionair, onChange }) 
 
         <input type="checkbox" checked={selected} onChange={(e) => setSelectedDataSource(e.target.checked)}></input>
 
+        {childItemsQ.map((childQ) => {
+            return (
+                <QuestionairGroupResponseData questionair={questionairUtility.getChildQuestionairByTag(childQ, node_COMMONCONSTANT.STORYDESIGN_QUESTION_TAG_DATASOURCERESPONSEDATA)} onChange={onChange} />
+            );
+        })}
 
     </div>
   );
